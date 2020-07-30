5b5f9e406beb7ac646845eea1c511c8a7b2eb57e
refaat31 <refaatalamgi@iut-dhaka.edu>
2020-07-30 23:26:28 +0600

    Modified progress bar :D

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
 
-const ProgressBar = () =>{
+const ProgressBar = ({file,setFile,msg,setMsg}) =>{
+    const {url,progress} = useStorage(file);
+    console.log(progress,url);
+
+    useEffect(()=>{
+        if(url) {
+            setFile(null);
+            setMsg('Uploaded');
+            setTimeout(()=>{setMsg(null)},3000);
+        }
+    },[url,setFile,setMsg]);
     return(
-        <div >
-            progress
-        </div>
+        <div className="progress-bar" style={{width:progress + '%'}}></div>
+        
     )
 }
 
--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -4,15 +4,18 @@ import ProgressBar from './ProgressBar';
 function UploadForm() {
     const [file,setFile] = useState(null);
     const [error,setError] = useState(null);
+    const [msg,setMsg] = useState(null);
     const types = ['image/png','image/jpeg'];
     const changeHandler = (e) =>{
         let selected = e.target.files[0];
         if(selected && types.includes(selected.type)){
             setFile(selected);
+            setMsg('Uploading');
             setError('');
         }
         else{
             setFile(null);
+            setMsg(null);
             setError('Please select an image file (png or jpeg) ');
         }
         console.log(file);
@@ -27,8 +30,10 @@ function UploadForm() {
             <div className="output">
                 {error && <div className="error">{error}</div>}
                 {file && <div>{file.name}</div> }
-                {file && <ProgressBar file={file} setFile={setFile}/>}
+                {file && <ProgressBar file={file} setFile={setFile} msg={msg} setMsg={setMsg}/>}
+
             </div>
+            <h5>{msg}</h5>
         </form>
     )
 }
