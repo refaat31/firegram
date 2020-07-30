87d6ba3d69d38a0830a257097aedfb1607ba54b0
refaat31 <refaatalamgi@iut-dhaka.edu>
2020-07-30 20:35:18 +0600

    Index.css copied from upstream. Fancy upload button. Incomplete ProgressBar.

---
+++ src/comps/ProgressBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import useStorage from '../hooks/useStorage';
+
+const ProgressBar = () =>{
+    return(
+        <div >
+            progress
+        </div>
+    )
+}
+
+export default ProgressBar;
--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import ProgressBar from './ProgressBar';
 
 function UploadForm() {
     const [file,setFile] = useState(null);
@@ -18,11 +19,15 @@ function UploadForm() {
     }
     return (
         <form>
+            <label>
             <input type="file" onChange={changeHandler}/>
+            <span>+</span>
+            </label>
+            
             <div className="output">
                 {error && <div className="error">{error}</div>}
                 {file && <div>{file.name}</div> }
-
+                {file && <ProgressBar file={file} setFile={setFile}/>}
             </div>
         </form>
     )
