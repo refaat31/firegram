bd57c86d0ecba093b29a1e8adec2835a76cccb5c
refaat31 <refaatalamgi@iut-dhaka.edu>
2020-07-31 01:27:01 +0600

    Modal added for enlarged view

---
+++ src/comps/Modal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Modal({selectedImg,setSelectedImg}) {
+    const handleClick = (e) =>{
+        if(e.target.classList.contains('backdrop')) setSelectedImg(null);
+    }
+    return (
+        <div className="backdrop" onClick={handleClick}>
+            <img src={selectedImg} alt="enlarged"></img>
+        </div>
+    )
+}
+
+export default Modal;
